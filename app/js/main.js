a849ac479fd63a529972491e009e0bfb3ee97672
MSAkain <theggodisthere@gmail.com>
2022-01-04 10:18:32 +0300

    Init

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+
+	$('.reviews__slider').slick({
+		fade: true,
+		infinite: false,
+		prevArrow: '<button type="button" class="slick-arrow arrow-right"><img src="images/icons/arrow-1.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-arrow arrow-left"><img src="images/icons/arrow-2.svg" alt=""></button>',
+	});
+
+
+
+});
