e32c576c6093afa8bbbf0072e02ad7432256aa8a
MSAkain <theggodisthere@gmail.com>
2022-01-05 10:12:56 +0300

    added header and swiper

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,7 @@
 $(function () {
 
-
-	$('.reviews__slider').slick({
-		fade: true,
-		infinite: false,
-		prevArrow: '<button type="button" class="slick-arrow arrow-right"><img src="images/icons/arrow-1.svg" alt=""></button>',
-		nextArrow: '<button type="button" class="slick-arrow arrow-left"><img src="images/icons/arrow-2.svg" alt=""></button>',
+	new Swiper('.header__swiper', {
+		slidesPerView: 1.6,
 	});
-
-
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ function styles() {
 function scripts() {
 	return src([
 		'node_modules/jquery/dist/jquery.js',
-		'node_modules/slick-carousel/slick/slick.js',
+		'node_modules/swiper/swiper-bundle.min.js',
 		'app/js/main.js'
 	])
 		.pipe(concat('main.min.js'))
