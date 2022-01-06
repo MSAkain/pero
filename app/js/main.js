5d8f46b1d58a32f9ac4e309bd4a6ef53eb812586
MSAkain <theggodisthere@gmail.com>
2022-01-06 10:42:19 +0300

    added section with slider

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,17 @@ $(function () {
 		slidesPerView: 1.6,
 	});
 
+	new Swiper('.popular__swiper', {
+		slidesPerView: 3.5,
+		spaceBetween: 20,
+		navigation: {
+			nextEl: '.swiperNext',
+			prevEl: '.swiperPrev'
+		},
+		scrollbar: {
+			el: '.swiper-scrollbar',
+			draggable: true
+		},
+	});
+
 });
